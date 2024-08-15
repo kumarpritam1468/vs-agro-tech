07cc3030486bb6f92a950112af46ec2c30f6727e
Pritam <manoharipritam@gmail.com>
2024-08-15 21:08:58 +0530

    first commit with navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,45 @@
+'use client'
+
+import React, { useState } from 'react'
+import { usePathname } from 'next/navigation';
+import { Squash as Hamburger } from 'hamburger-react'
+
+const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className='fixed top-0 bg-[#66C15F] h-[10svh] w-full flex justify-between items-center px-12 max-md:px-6'>
+      <img src="/logo.png" alt="Logo" className=' w-20' />
+
+      <div className='navlinks flex gap-1 max-md:hidden'>
+        <a href='/' className={`${pathname === '/' ? 'active' : ''}`}>Home</a>
+        <a href='/products' className={`${pathname === '/products' ? 'active' : ''}`}>Products</a>
+        <a href='/gallery' className={`${pathname === '/gallery' ? 'active' : ''}`}>Gallery</a>
+        <a href='/about' className={`${pathname === '/about' ? 'active' : ''}`}>About</a>
+        <a href='/contact' className={`${pathname === '/contact' ? 'active' : ''}`}>Contact Us</a>
+      </div>
+      <div className='md:hidden'>
+        <Hamburger size={25} onToggle={toggled => {
+          if (toggled) {
+            setMenuOpen(true)
+          } else {
+            setMenuOpen(false)
+          }
+        }} />
+      </div>
+
+      <div className={`navlinks absolute top-[12vh] rounded-3xl md:hidden px-8 py-5 flex flex-col items-center gap-1 bg-[#66C15F] transition-all duration-300 ease-in-out ${menuOpen ? 'right-4' : '-right-60'}`}>
+        <a href='/' className={`${pathname === '/' ? 'active' : ''}`}>Home</a>
+        <a href='/products' className={`${pathname === '/products' ? 'active' : ''}`}>Products</a>
+        <a href='/gallery' className={`${pathname === '/gallery' ? 'active' : ''}`}>Gallery</a>
+        <a href='/about' className={`${pathname === '/about' ? 'active' : ''}`}>About</a>
+        <a href='/contact' className={`${pathname === '/contact' ? 'active' : ''}`}>Contact Us</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "VS Agro Tech",
+  description: "With a vision to provide healthy and good to the animals,  Go seva learnt from the teaching of lord Krishna,urea and chemical based feed",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} pt-[10svh]`}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
