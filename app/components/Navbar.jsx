368fb9a9180a365729c641c18c2d3b447761b6ea
Pritam <manoharipritam@gmail.com>
2024-10-01 20:58:56 +0530

    update logo

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -39,9 +39,9 @@ const Navbar = () => {
 
       <div className='navlinks flex gap-3 max-md:hidden'>
         <a href='/' className={`${pathname === '/' ? 'active' : ''}`}>Home</a>
-        <Link href='/products' className={`${pathname === '/products' ? 'active' : ''}`}>Products</Link>
-        <Link href='/about' className={`${pathname === '/about' ? 'active' : ''}`}>About</Link>
-        <Link href='/contact' className={`${pathname === '/contact' ? 'active' : ''}`}>Contact Us</Link>
+        <a href='/products' className={`${pathname === '/products' ? 'active' : ''}`}>Products</a>
+        <a href='/about' className={`${pathname === '/about' ? 'active' : ''}`}>About</a>
+        <a href='/contact' className={`${pathname === '/contact' ? 'active' : ''}`}>Contact Us</a>
       </div>
       <div className='md:hidden'>
         <Hamburger aria-label="Menu" size={25} color='#ffffff' onToggle={toggled => {
@@ -56,9 +56,9 @@ const Navbar = () => {
       <div className={`navlinks navlinksPhn absolute w-[60vw] h-[90dvh] top-[10svh] md:hidden pl-8 py-10 flex flex-col justify-between bg-white border-l-2 border-l-gray-600 transition-all duration-300 ease-in-out ${menuOpen ? 'right-0' : '-right-[60vw]'}`}>
         <div className=' flex flex-col gap-4'  onClick={() => setMenuOpen(prev => !prev)}>
           <a href='/' className={`${pathname === '/' ? 'active' : ''}`}>Home</a>
-          <Link href='/products' className={`${pathname === '/products' ? 'active' : ''}`}>Products</Link>
-          <Link href='/about' className={`${pathname === '/about' ? 'active' : ''}`}>About</Link>
-          <Link href='/contact' className={`${pathname === '/contact' ? 'active' : ''}`}>Contact Us</Link>
+          <a href='/products' className={`${pathname === '/products' ? 'active' : ''}`}>Products</a>
+          <a href='/about' className={`${pathname === '/about' ? 'active' : ''}`}>About</a>
+          <a href='/contact' className={`${pathname === '/contact' ? 'active' : ''}`}>Contact Us</a>
         </div>
 
         <div className=' flex flex-col w-1/2 max-md:w-full'>
